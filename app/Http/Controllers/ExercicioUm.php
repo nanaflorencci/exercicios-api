1dec5636b5f65aa844e7b018a7abc73444fb2465
nanaflorencci <giovannaflorencci1814@gmail.com>
2023-08-01 10:29:54 -0300

    Exercício 1

--- app/Http/Controllers/ExercicioUm.php
+++ app/Http/Controllers/ExercicioUm.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 
 class ExercicioUm extends Controller
 {
-    public function multiplicar(Request $request){
+    public function Multiplicar(Request $request)
+    {
         $primeiraEntrada = $request->primeiro_numero;
-        $segundaEntrada = $request->segundo_numero;   
+        $segundaEntrada = $request->segundo_numero;
 
         return json_encode([
             'resultado' => $primeiraEntrada * $segundaEntrada
-        ]); 
+        ]);
     }
 }
